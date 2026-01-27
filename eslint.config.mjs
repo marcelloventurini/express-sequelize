5a9195b96c81a0fc78db64a5ffaf5b399fc255ec
Marcello Venturini <marcellofventurini@gmail.com>
2026-01-27 11:27:34 -0300

    chore: update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      indent: ['warn', 2],
+      quotes: ['warn', 'single'],
+      semi: ['warn', 'always'],
+    },
+  },
 ]);
