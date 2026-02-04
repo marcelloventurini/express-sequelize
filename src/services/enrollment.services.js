dad79ae2263a725d1ab14e2f9b58bc94510ca902
Marcello Venturini <marcellofventurini@gmail.com>
2026-02-04 12:24:06 -0300

    feat: create services for category, course and enrollment

---
+++ src/services/enrollment.services.js
@@ -0,0 +1,9 @@
+const Services = require('./services.js');
+
+class EnrollmentServices extends Services {
+  constructor() {
+    super('Enrollment');
+  }
+}
+
+module.exports = EnrollmentServices;
